49a2fb057a3c0a325780997f02f7af8137b21015
pramudarshan <80111513+pramudarshan@users.noreply.github.com>
2021-03-05 22:44:33 +0530

    Add files via upload

---
+++ calculator_in_switch.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+float a,b,res;
+char d;
+printf("enter the number");
+scanf("%f%c%f",&a,&d,&b);
+switch(d)
+{
+case'+':
+res=a+b;
+break;
+case'-':
+res=a-b;
+break;
+case'*':
+res=a*b;
+break;
+case'/':
+res=a/b;
+break;
+}
+}
